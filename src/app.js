ce8a7d86a2a428b9d23a92df4f7db703a82c5eea
Florent Le Gall <florent.legall@gmail.com>
2015-11-21 21:38:29 +0100

    Add basic counter

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import Yolk from 'yolk'
+
+function Counter() {
+
+  // map all plus button click events to 1
+  const handlePlus = this.createEventHandler();
+  const plusOne = handlePlus.map(() => 1);
+
+  // map all minus button click events to -1
+  const handleMinus = this.createEventHandler();
+  const minusOne = handleMinus.map(() => -1);
+
+  // merge both event streams together and keep a running count of the result
+  const count = plusOne.merge(minusOne).scan((x, y) => x + y, 0).startWith(0);
+
+  return (
+    <div>
+      <div>
+        <button id="plus" onClick={handlePlus}>+</button>
+        <button id="minus" onClick={handleMinus}>-</button>
+      </div>
+      <div>
+        <span>Count: {count}</span>
+      </div>
+    </div>
+  );
+}
+
+Yolk.render(<Counter />, document.getElementById('container'));
